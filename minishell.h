b541867b20259f8c996e071a99a7f64f218551ca
Luis Palomino Morón <lpalominom4c@gmail.com>
2025-09-27 12:12:13 +0200

    small error with readline library

--- minishell.h
+++ minishell.h
@@ -14,13 +14,13 @@
 # define MINISHELL_H
 
 # include "libft/libft.h"
+# include <stdio.h>
 # include <errno.h>
 # include <fcntl.h>
 # include <linux/limits.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/wait.h>
